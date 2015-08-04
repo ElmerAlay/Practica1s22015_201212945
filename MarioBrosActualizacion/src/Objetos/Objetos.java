c67a7b08ec543cea26b0a912a679f690852a7d1e
ElmerAlay <alayelmer1993@gmail.com>
2015-08-03 22:13:25 -0600

    MarioBrosActualizacion
    Actualizacion 1

---
+++ MarioBrosActualizacion/src/Objetos/Objetos.java
@@ -0,0 +1,15 @@
+
+package Objetos;
+
+import javax.swing.ImageIcon;
+
+public class Objetos {
+    
+    String tipo;
+    ImageIcon icono;
+    
+    public Objetos(String tipo, ImageIcon icono){
+        this.tipo=tipo;
+        this.icono=icono;
+    }
+}
